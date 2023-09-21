520fcdfb7c25eff744f29643c130ec3bec1f2cf2
Hunain Hyder <65592359+hunainhyder@users.noreply.github.com>
2023-09-21 18:44:05 +0000

    layout changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
+        <div className='max-w-3xl mx-auto p-4'>
+          <Navbar />
+        </div>
         {children}
       </body>
     </html>
