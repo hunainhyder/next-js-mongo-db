78800d6f78a4c673d92b1e0bef4b2cf3490bcfd4
Hunain Hyder <65592359+hunainhyder@users.noreply.github.com>
2023-09-21 20:18:18 +0000

    question list styling

--- components/QuestionsList.jsx
+++ components/QuestionsList.jsx
@@ -2,7 +2,7 @@ import RemoveBtn from '@/components/RemoveBtn'
 
 export default function QuestionsList() {
     return <>
-        <div>
+        <div className='p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start'>
             <div>
                 <h2>Question</h2>
                 <div>Answers</div>
