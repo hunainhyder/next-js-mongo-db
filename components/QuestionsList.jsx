861edd8e275a60362486f82c14afceaf8d82706b
Hunain Hyder <65592359+hunainhyder@users.noreply.github.com>
2023-09-21 20:16:36 +0000

    some changes in styling

---
+++ components/QuestionsList.jsx
@@ -0,0 +1,20 @@
+import RemoveBtn from '@/components/RemoveBtn'
+
+export default function QuestionsList() {
+    return <>
+        <div>
+            <div>
+                <h2>Question</h2>
+                <div>Answers</div>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+            <div>
+                <RemoveBtn />
+            </div>
+        </div>
+    </>
+}
---
+++ components/RemoveBtn.jsx
@@ -0,0 +1,7 @@
+import { HiOutlineTrash } from 'react-icons/hi'
+
+export default function removeBtn() {
+    return <button className='text-red-400'>
+        <HiOutlineTrash size={24} />
+    </button>
+}
