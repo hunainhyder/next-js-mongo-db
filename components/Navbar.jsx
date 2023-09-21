d652507aee75da8521ee3028d74c601ae9be0a6d
Hunain Hyder <65592359+hunainhyder@users.noreply.github.com>
2023-09-21 20:28:10 +0000

    navbar title changed

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Navbar() {
     return <nav className='flex justify-between items-center bg-slate-800 px-8 py-3'>
-        <Link className="text-white font-bold" href={"/"}>Home</Link>
+        <Link className="text-white font-bold" href={"/"}>Quiz Application Backend</Link>
         <div>
             <Link className="bg-white p-2 ml-1" href={"/addQuestion"}>Add Questions</Link>
             <Link className="bg-white p-2 ml-1" href={"/addSection"}>Add Category</Link>
