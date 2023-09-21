7da94143b9b37fb6eca5f1363ebbd91c903b4fac
Hunain Hyder <65592359+hunainhyder@users.noreply.github.com>
2023-09-21 19:58:21 +0000

    changes in navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
 
 export default function Navbar() {
-    return <nav>
-        <Link href={"/"}>Home</Link>
-        <Link href={"/addQuestion"}>Add Questions</Link>
-        <Link href={"/addSection"}>Add Category</Link>
+    return <nav className='flex justify-between items-center bg-slate-800 px-8 py-3'>
+        <Link className="text-white font-bold" href={"/"}>Home</Link>
+        <Link className="bg-white p-2" href={"/addQuestion"}>Add Questions</Link>
+        <Link className="bg-white p-2" href={"/addSection"}>Add Category</Link>
     </nav>
 }
